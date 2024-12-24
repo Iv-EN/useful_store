ed53066a47ec977121d1b546531f0d46de860265
Iv-EN <engenivanov@yandex.ru>
2024-12-24 22:33:49 +0300

    create Product and Category models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """Описывает продукт."""
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="products/", verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        "Category",
+        verbose_name="Категория",
+        related_name="products",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Цена"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата изменения"
+    )
+
+    def __str__(self):
+        return f"Продукт: {self.name} категория: {self.category.name}"
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["-created_at"]
+
+
+class Category(models.Model):
+    """Описывает категорию продукта."""
+
+    name = models.CharField(max_length=100, verbose_name="Название категории")
+    description = models.TextField(verbose_name="Описание категории")
+
+    def __str__(self):
+        return f"Категория: {self.name}"
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
