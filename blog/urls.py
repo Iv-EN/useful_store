7d575dc132aa61bc731daec37815ea5b2c4340ef
Iv-EN <engenivanov@yandex.ru>
2025-01-12 14:34:53 +0300

    changed the address of the blog registration controller and the readme.md file

--- blog/urls.py
+++ blog/urls.py
@@ -13,12 +13,12 @@ app_name = "blog"
 urlpatterns = [
     path("create/", BlogPostCreateView.as_view(), name="post_create"),
     path("", BlogPostListView.as_view(), name="post_list"),
-    path("blog/<int:pk>/", BlogPostDetailView.as_view(), name="post_detail"),
+    path("blogs/<int:pk>/", BlogPostDetailView.as_view(), name="post_detail"),
     path(
-        "blog/<int:pk>/edit/", BlogPostUpdateView.as_view(), name="post_edit"
+        "blogs/<int:pk>/edit/", BlogPostUpdateView.as_view(), name="post_edit"
     ),
     path(
-        "blog/<int:pk>/delete/",
+        "blogs/<int:pk>/delete/",
         BlogPostDeleteView.as_view(),
         name="post_delete",
     ),
